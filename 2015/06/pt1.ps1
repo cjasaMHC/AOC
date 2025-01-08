e7993a77a08b450e166e932e7d50198bfe9ee909
Caleb Jasa <cjasa@midtownhealthne.org>
2025-01-08 17:46:21 -0600

    lets get functional. functional

--- 2015/06/pt1.ps1
+++ 2015/06/pt1.ps1
@@ -8,7 +8,7 @@ function Create-Grid {
       [void]$lightGrid.Add([PSCustomObject]@{
           x = $x
           y = $y
-          state = 0
+          state = $false
         })
     }
   }
@@ -16,26 +16,59 @@ function Create-Grid {
   return $lightGrid
 }
 
-$in | ForEach-Object {
-  #$charArr = $_.ToCharArray()
-  # what to do
-  $line = $_
-  $instruction = $_.Substring(0,7)
-  # where to do it
-  # toggle and on 7, off 8
+function do-instruction {
+  param(
+    [string]$method,
+    [int]$xStart,
+    [int]$yStart,
+    [int]$xEnd,
+    [int]$yEnd,
+    [System.Collections.ArrayList]$grid
+  )
 
-  switch ($instruction) {
-    'toggle ' {
-      $coords = $line.Substring(7).Trim()
-    }
-    'turn on' {
-      $coords = $line.Substring(7).Trim()
-    }
-    'turn of' {
-      $coords = $line.Substring(8).Trim()
-    }
-  }
 }
 
-$grid = create-grid
-write-host $($grid.Count)
+function process-line {
+  param(
+    [string]$in
+  )
+  $insList = New-Object System.Collections.ArrayList
+
+  $in | ForEach-Object {
+    $line = $_
+    $instruction = $_.Substring(0,7)
+
+    switch ($instruction) {
+      'toggle ' {
+        $coords = $line.Substring(7).Trim()
+      }
+      'turn on' {
+        $coords = $line.Substring(7).Trim()
+      }
+      'turn of' {
+        $coords = $line.Substring(8).Trim()
+      }
+    }
+    $xycoords = $coords -split ' through ',2
+    $beginning = $xycoords[0] -split ',',2
+    $end = $xycoords[1] -split ',',2
+    $xBeg = $beginning[0]
+    $yBeg = $beginning[1]
+    $xEnd = $end[0]
+    $yEnd = $end[1]
+    $ins = [PSCustomObject]@{
+      method = $instruction
+      xBeg = $xBeg
+      xEnd = $xEnd
+      yBeg = $yBeg
+      yEnd = $yEnd
+    }
+    $insList.Add($ins) | Out-Null
+  }
+
+  return $insList
+}
+
+
+$test = $in | ForEach-Object { process-line -in $_ }
+Write-Host $test
